893951e67000cccd4a20440b476188f30f3e9d34
Deepak <121931668+Deepak-0007@users.noreply.github.com>
2023-06-16 23:33:28 +0530

    Add files via upload

---
+++ a.java
@@ -0,0 +1,14 @@
+public class a{
+    public static void main(String args[]){
+        
+    
+    int arr[]=new int[]{1,1,2,2,3,4,8,9,5,3};
+    for(int i=0; i<arr.length; i++){
+        for(int j=i+1;j<arr.length;j++){
+            if(arr[i]==arr[j])
+            System.out.println(arr[j]);
+        }
+    }
+    
+}
+}
