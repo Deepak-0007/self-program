cc30b34bb5ef0c9eab459044e57faca4ccbdcb79
Deepak <121931668+Deepak-0007@users.noreply.github.com>
2023-11-03 23:43:55 +0530

    find min num thorugh linear search

---
+++ minarrya.java
@@ -0,0 +1,15 @@
+public class minarrya {
+    public static void main(String[] args) {
+        int [] arr={19,18,-7,2,1,};
+        System.out.println(min(arr));
+    }
+    static int min(int []arr){
+        int ans=arr[0];
+        for (int i=1; i< arr.length; i++){
+            if(arr[i]<ans){
+                ans=arr[i];
+            }
+        }
+        return ans;
+    }
+}
