d66ff941c6552747918eccdfde543a50222fd48e
Deepak <121931668+Deepak-0007@users.noreply.github.com>
2023-11-16 23:15:54 +0530

    order agno.. to find number by binary search

---
+++ binarysearch2.java
@@ -0,0 +1,48 @@
+public class binarysearch2 {
+    public static void main(String[] args) {
+       // int arr[]={-12,0,2,4,5,6,7,8,9,12,13,14};
+        int arr[]={12,11,10,9,7};
+        int target=12;
+        int ans=binarySearch(arr,target);
+        System.out.println("location of target element by binary search is "+ans);
+    }
+    static int binarySearch(int arr[] , int target){
+        int start=0;
+        int end=arr.length-1;
+
+
+        boolean isAsc;
+        if(arr[start]<arr[end]){
+            isAsc=true;
+        }else{
+            isAsc=false;
+        }
+
+
+         
+        while (start<=end) {
+            int mid=start+(end-start)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+
+
+            if(isAsc){
+                if (target<arr[mid]) {
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+            }
+            else{
+                if (target>arr[mid]) {
+                end=mid-1;
+            }else {
+                start=mid+1;
+            }
+            }
+        }
+        return -1;
+    }
+}
+
