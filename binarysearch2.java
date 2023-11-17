9ef510cda8c629afa24e6636198b300c382fad97
Deepak <121931668+Deepak-0007@users.noreply.github.com>
2023-11-17 23:36:22 +0530

    Update binarysearch2.java

--- binarysearch2.java
+++ binarysearch2.java
@@ -2,7 +2,7 @@ public class binarysearch2 {
     public static void main(String[] args) {
        // int arr[]={-12,0,2,4,5,6,7,8,9,12,13,14};
         int arr[]={12,11,10,9,7};
-        int target=12;
+        int target=11;
         int ans=binarySearch(arr,target);
         System.out.println("location of target element by binary search is "+ans);
     }
