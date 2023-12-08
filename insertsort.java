23fdc474857627b078390dc42c0afb48b817fadc
Deepak <121931668+Deepak-0007@users.noreply.github.com>
2023-12-08 23:00:12 +0530

    Update insertsort.java

--- insertsort.java
+++ insertsort.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class insertsort {
      public static void main(String[] args) {
-        int []arr={6,4,1,5,3,2};
+        int []arr={6,4,1,5,3,2,8};
         insert(arr);
         System.out.println(Arrays.toString(arr));
         
