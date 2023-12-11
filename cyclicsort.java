5b55b767b7260c380e90c7315b987eec5413a4a1
Deepak <121931668+Deepak-0007@users.noreply.github.com>
2023-12-11 22:23:31 +0530

    Update cyclicsort.java

--- cyclicsort.java
+++ cyclicsort.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class cyclicsort {
     public static void main(String[] args) {
-        int []arr={3,4,2,1,5};
+        int []arr={3,4,0,2,1,5};
         sort(arr);
         System.out.println(Arrays.toString(arr));
         
