e12deb2fa17ac759c28eb67af55a367332c1984c
Deepak <121931668+Deepak-0007@users.noreply.github.com>
2024-01-01 22:29:22 +0530

    Update array.java

--- array.java
+++ array.java
@@ -2,7 +2,7 @@ public class array{
     public static void main(String args[]){
         
     
-    int arr[]=new int[]{1,1,2,2,3,4,8,9,5,3};
+    int arr[]=new int[]{1,1,2,2,3,4,8,5,5,3};
     for(int i=0; i<arr.length; i++)
     System.out.println(arr[i]);
 }
