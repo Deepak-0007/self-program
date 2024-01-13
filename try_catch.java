e58876d6a7d8baeab49e65ce3e45be008092d1a6
Deepak <121931668+Deepak-0007@users.noreply.github.com>
2024-01-13 22:39:21 +0530

    try catch simple program

---
+++ try_catch.java
@@ -0,0 +1,11 @@
+class try_catch{
+    public static void main(String[] args) {
+        try {
+            int a=50/0;
+            System.out.println("divide");
+        } catch (Exception e) {
+            System.out.println("can't divide by zero");
+            // TODO: handle exception
+        }
+    }
+}
